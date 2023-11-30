e97188f5a9c7ed17dcdd09afcb4df37c1fe11438
EudlayFelipe <103539515+EudlayFelipe@users.noreply.github.com>
2023-11-30 13:07:09 -0300

    :tada: Upd

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector('.h')
     header.classList.toggle('rolagem' ,window.scrollY > 0)
 })
+
+
+
+function submitForm(event) {
+    event.preventDefault();
+   
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+  
+    alert("Nome: " + name + "\nEmail: " + email + "\nIdade: " + age);
+  }
